09586b6de2baf0aa4bde0b8b8fd57f7e4dd29f9c
nan <nan>
2022-10-31 23:33:09 +0800

    nonsense

--- create-dataset-links.sh
+++ create-dataset-links.sh
@@ -4,5 +4,7 @@
 printf "Creating symbolic link to dataset directory..."
 # get directory name of current file
 curdir=$(dirname "$0")
+# convert curdir to absolute path
+curdir=$(cd "$curdir" && pwd)
 printf '\nln -s ${curdir}/dataset $HOME/.dataset\n'
 ln -s ${curdir}/dataset $HOME/.dataset
